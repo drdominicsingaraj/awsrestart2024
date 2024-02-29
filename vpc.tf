37865fde154e281832ac5f8b168981eb069a8f5d
Dominic Singaraj <dominic.singaraj.deutschland@gmail.com>
2024-02-29 11:41:09 +0100

    qa

--- vpc.tf
+++ vpc.tf
@@ -25,7 +25,7 @@ module "vpc-qa" {
 
   azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
   private_subnets = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-  public_subnets  = ["10.0.99.0/24", "10.0.98.0/24", "10.0.97.0/24"]
+  public_subnets  = ["10.0.7.0/24", "10.0.8.0/24", "10.0.9.0/24"]
 
   enable_nat_gateway = false
   enable_vpn_gateway = false
