b19af3cc541d3dd4e5ae4e46243fbac031afd437
Dominic Singaraj <dominic.singaraj.deutschland@gmail.com>
2024-02-29 11:39:10 +0100

    myvpc1

--- vpc.tf
+++ vpc.tf
@@ -20,12 +20,12 @@ module "vpc" {
 module "vpc-qa" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "my-vpc"
+  name = "my-vpc1"
   cidr = "10.0.0.0/20"
 
-  azs             = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
   private_subnets = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = ["10.0.99.0/24", "10.0.98.0/24", "10.0.97.0/24"]
 
   enable_nat_gateway = false
   enable_vpn_gateway = false
